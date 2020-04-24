e2893a5c64e42298c889d05adba7965b5d1ea1b7
DavHau <hsngrmpf+github@gmail.com>
2020-04-24 04:24:56 +0000

    default.nix return attributes

--- default.nix
+++ default.nix
@@ -3,11 +3,22 @@ let
   python = import ./mach_nix/nix/python.nix { inherit pkgs; };
   python_deps = (pkgs.lib.attrValues (import ./mach_nix/nix/python-deps.nix { inherit python; fetchurl = pkgs.fetchurl; }));
 in
-python.pkgs.buildPythonPackage rec {
-  pname = "mach-nix";
-  version = builtins.readFile ./mach_nix/VERSION;
-  name = "${pname}-${version}";
-  src = ./.;
-  propagatedBuildInputs = python_deps;
-  doCheck = false;
+rec {
+  mach-nix = python.pkgs.buildPythonPackage rec {
+    pname = "mach-nix";
+    version = builtins.readFile ./mach_nix/VERSION;
+    name = "${pname}-${version}";
+    src = ./.;
+    propagatedBuildInputs = python_deps;
+    doCheck = false;
+  };
+
+  # call this to generate a nix expression defnining a python environment
+  mkPythonExpr = args: import ./mach_nix/nix/expression.nix args;
+
+  # call this to generate a python environment
+  mkPython = args: import "${mkPythonExpr args}/share/expr.nix";
+
+  # call this to use the python environment with nix-shell
+  mkPythonShell = args: (mkPython args).env;
 }
