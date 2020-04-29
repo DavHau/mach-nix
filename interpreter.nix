1d184b8e79d1df8f42ff726572be9a0f4d4b3093
DavHau <hsngrmpf+github@gmail.com>
2020-04-29 17:41:05 +0000

    fix interpreter.nix

---
+++ interpreter.nix
@@ -0,0 +1,4 @@
+# python interpreter for dev environment
+import ./mach_nix/nix/python.nix {
+  pkgs = import (import ./mach_nix/nix/nixpkgs-src.nix).stable { config = {}; };
+}
--- nix/interpreter.nix
+++
@@ -1,1 +0,0 @@
-import ./python.nix {pkgs = import (import ./nixpkgs-src.nix).stable { config = {}; }; }
