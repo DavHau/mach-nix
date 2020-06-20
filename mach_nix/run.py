DavHau/mach-nix

commit fa2bb2d33fb9b9dc3113046e4fcc16088f56981a

Improvements (#3)
* edd print version and improve UI

* default.nix return attributes

* add readme

* fix readme

mach_nix/run.py
@@ -10,20 +10,21 @@ from mach_nix.ensure_nix import ensure_nix
 from mach_nix.versions import PyVer
 
 
-def gen(args, quiet: bool, return_expr=False):
+pwd = dirname(realpath(__file__))
+
+
+def gen(args, return_expr=False):
     with open(args.r) as f:
         requirements = f.read().strip()
-    pwd = dirname(realpath(__file__))
     o_file = tempfile.mktemp()
     py_ver = PyVer(args.python)
     cmd = f'nix-build {pwd}/nix/expression.nix -o {o_file}' \
           f' --argstr requirements "{requirements}"' \
           f' --argstr python_attr python{py_ver.digits()}' \
           f' --arg prefer_nixpkgs {json.dumps((not args.prefer_new))}'
-    proc = sp.run(cmd, shell=True, capture_output=quiet)
+    proc = sp.run(cmd, shell=True, stdout=sys.stderr)
     if proc.returncode:
-        if quiet:
-            print(proc.stderr.decode(), file=sys.stderr)
+        print(proc.stderr.decode(), file=sys.stderr)
         exit(1)
     with open(f"{o_file}/share/expr.nix") as src:
         expr = src.read()
@@ -39,7 +40,7 @@ def gen(args, quiet: bool, return_expr=False):
 
 def env(args):
     target_dir = args.directory
-    expr = gen(args, quiet=False, return_expr=True)
+    expr = gen(args, return_expr=True)
     python_nix_file = f"{target_dir}/python.nix"
     shell_nix_file = f"{target_dir}/shell.nix"
     default_nix_file = f"{target_dir}/default.nix"
@@ -58,9 +59,9 @@ def env(args):
           f"To activate it, execute: 'nix-shell {target_dir}'")
 
 
-def be_patient():
+def print_be_patient():
     print("Generating python environment... If you run this the first time, the python package index "
-          "and dependency graph (~200MB) need to be downloaded. Please stay patient!")
+          "and dependency graph (~200MB) need to be downloaded. Please stay patient!", file=sys.stderr)
 
 
 def main():
@@ -81,7 +82,8 @@ def main():
                  'This might increase build times significantly since no cache can be used'))
     )
     parser = ArgumentParser()
-    subparsers = parser.add_subparsers(dest='command', required=True)
+    parser.add_argument('--version', '-V', help='show program version', action='store_true')
+    subparsers = parser.add_subparsers(dest='command')
 
     gen_parser = subparsers.add_parser('gen', help='generate a nix expression')
     gen_parser.add_argument('-o', help='output file. defaults to stdout', metavar='python.nix')
@@ -95,11 +97,20 @@ def main():
 
     args = parser.parse_args()
 
+    if args.version:
+        with open(f"{pwd}/VERSION") as f:
+            print(f"mach-nix: {f.read()}")
+            exit(0)
+
+    if args.command not in ('gen', 'env'):
+        parser.print_usage()
+        exit(1)
+
     ensure_nix()
-    be_patient()
+    print_be_patient()
 
     if args.command == 'gen':
-        gen(args, quiet=not args.o)
+        gen(args)
     elif args.command == 'env':
         env(args)
 

default.nix
@@ -3,11 +3,22 @@ let
   python = import ./mach_nix/nix/python.nix { inherit pkgs; };
   python_deps = (pkgs.lib.attrValues (import ./mach_nix/nix/python-deps.nix { inherit python; fetchurl = pkgs.fetchurl; }));
 in
-python.pkgs.buildPythonPackage rec {
-  pname = "mach-nix";
-  version = builtins.readFile ./VERSION;
-  name = "${pname}-${version}";
-  src = ./.;
-  propagatedBuildInputs = python_deps;
-  doCheck = false;
+rec {
+  mach-nix = python.pkgs.buildPythonPackage rec {
+    pname = "mach-nix";
+    version = builtins.readFile ./mach_nix/VERSION;
+    name = "${pname}-${version}";
+    src = ./.;
+    propagatedBuildInputs = python_deps;
+    doCheck = false;
+  };
+
+  # call this to generate a nix expression defnining a python environment
+  mkPythonExpr = args: import ./mach_nix/nix/expression.nix args;
+
+  # call this to generate a python environment
+  mkPython = args: import "${mkPythonExpr args}/share/expr.nix";
+
+  # call this to use the python environment with nix-shell
+  mkPythonShell = args: (mkPython args).env;
 }

setup.py
@@ -3,7 +3,7 @@ from os.path import abspath, dirname
 from setuptools import setup, find_packages
 
 pwd = dirname(abspath(__file__))
-with open(pwd + '/VERSION') as f:
+with open(pwd + '/mach_nix/VERSION') as f:
     version = f.read().strip()
 
 setup(
@@ -19,8 +19,7 @@ setup(
             "mach-nix = mach_nix:main"
         ],
     },
-    package_data={'': ['nix/*']},
-    include_package_data=True,
+    package_data={'': ['nix/*', 'VERSION']},
     install_requires=[
         'distlib ~= 0.3.0',
         'packaging >= 19.0',

commit 8981746008715db0af04195b9eb669356d1091b7

Fix AttributeError when nix-build fails
Since commit fa2bb2d33fb9b9dc3113046e4fcc16088f56981a, the nix-build
subprocess is called without capture_output=True. Therefore, proc.stderr
is None. The subprocess stderr is already being dumped to stderr, so
there is no need for doing a print.

mach_nix/run.py
@@ -25,7 +25,6 @@ def gen(args, return_expr=False):
           f' --arg prefer_nixpkgs {json.dumps((not args.prefer_new))}'
     proc = sp.run(cmd, shell=True, stdout=sys.stderr)
     if proc.returncode:
-        print(proc.stderr.decode(), file=sys.stderr)
         exit(1)
     with open(f"{o_file}/share/mach_nix_file.nix") as src:
         expr = src.read()