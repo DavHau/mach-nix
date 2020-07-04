b523072885b68e082375aa115ce494572e59d835
DavHau <hsngrmpf+github@gmail.com>
2020-07-04 12:22:33 +0000

    improve readme and add examples.md

--- example/simple-usage-shell.nix
+++
@@ -1,8 +0,0 @@
-let
-  mach-nix = import (builtins.fetchGit {
-    url = "https://github.com/DavHau/mach-nix/";
-    ref = "2.0.1";
-  });
-in mach-nix.mkPythonShell {
-  requirements = builtins.readFile ./requirements.txt;
-}
