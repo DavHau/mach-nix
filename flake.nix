1f1bdb28a24a3126d877b772f94f3188d2ea52c7
DavHau <hsngrmpf+github@gmail.com>
2020-10-04 02:16:46 +0700

    rename nixpkgs-unstable and update flakes.lock

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
