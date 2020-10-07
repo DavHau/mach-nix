252b8b8a95f93e999df7a668d991ff7f44e71cf0
DavHau <hsngrmpf+github@gmail.com>
2020-10-07 12:19:02 +0700

    update deps-db and add twine to dev shell

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ with import (import ./mach_nix/nix/nixpkgs-src.nix) { config = {}; };
 mkShell {
   buildInputs = [
     (import ./mach_nix/nix/python.nix { inherit pkgs; })
+    python37Packages.twine
   ];
   shellHook = ''
     export PYTHONPATH=$(pwd)/
