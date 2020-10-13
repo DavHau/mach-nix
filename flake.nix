7867f268c45e42e6729e081269b9f8111ac19edf
shadowrylander <aiern@protonmail.com>
2020-10-13 13:57:20 -0400

    Replaced use of "self" with a recursive set, and applied "flattenTree" as per Nix flake[-utils] specification

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
   description = "Create highly reproducible python environments";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.url = "github:numtide/flake-utils/master";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -11,24 +11,22 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         mach-nix-default = import ./default.nix {inherit pkgs;};
-      in
+      in rec
       {
         devShell = import ./shell.nix {
           inherit pkgs;
         };
-        packages = rec {
+        packages = flake-utils.lib.flattenTree {
           mach-nix = mach-nix-default.mach-nix;
-          "with" = mach-nix-default.pythonWith;
-          pythonWith = mach-nix-default.pythonWith;
+          "with" = mach-nix-default."with";
           shellWith = mach-nix-default.shellWith;
           dockerImageWith = mach-nix-default.dockerImageWith;
         };
 
-        defaultPackage = self.packages."${system}".mach-nix.mach-nix;
+        defaultPackage = packages."${system}".mach-nix.mach-nix;
 
-        apps.mach-nix = flake-utils.lib.mkApp { drv = self.packages.mach-nix.mach-nix; };
-        defaultApp = { type = "app"; program = "${self.defaultPackage."${system}"}/bin/mach-nix"; };
+        apps.mach-nix = flake-utils.lib.mkApp { drv = packages.mach-nix.mach-nix; };
+        defaultApp = { type = "app"; program = "${defaultPackage."${system}"}/bin/mach-nix"; };
       }
   );
 }
-
