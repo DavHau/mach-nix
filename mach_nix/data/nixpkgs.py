f75bd25168232db620f2ddbe5c5281d14c17d03b
InLaw <marsweb@gmx.de>
2021-01-22 19:19:44 +0100

    Update nixpkgs.py (#218)

--- mach_nix/data/nixpkgs.py
+++ mach_nix/data/nixpkgs.py
@@ -19,7 +19,6 @@ class NixpkgsIndex(UserDict):
     # mapping from pypi name to nix key
     _aliases = dict(
         torch='pytorch',
-        tensorboard='tensorflowtensorboard'
     )
 
     def __init__(self, nixpkgs_json_file, **kwargs):
