10bdb376f6bd695a8711c2da1f2f4aed5a4335dd
jared-w <jaredweakly@gmail.com>
2021-10-20 13:22:09 -0700

    Don't hardcode nix flake tests to be x86_64-linux

--- tests/test_flakes_lib.nix
+++ tests/test_flakes_lib.nix
@@ -2,11 +2,12 @@
   baseArgsMkPython ? {},
   baseArgsBuildPythonPackage ? {},
   mach-nix ? import ../. {},
+  system ? builtins.currentSystem or "x86_64-linux",
   ...
 }:
 with builtins;
 let
-  mkPython = (builtins.getFlake (toString ../.)).lib.x86_64-linux.mkPython;
-  buildPythonPackage = (builtins.getFlake (toString ../.)).lib.x86_64-linux.buildPythonPackage;
+  mkPython = (builtins.getFlake (toString ../.)).lib.${system}.mkPython;
+  buildPythonPackage = (builtins.getFlake (toString ../.)).lib.${system}.buildPythonPackage;
 in
 mkPython [ "https://github.com/psf/requests/tarball/v2.25.0" ]
