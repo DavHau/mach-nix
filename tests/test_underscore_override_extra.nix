4dde445784e47a083d41e4c80f36e8a916f44f1f
DavHau <hsngrmpf+github@gmail.com>
2022-04-20 16:30:32 +0200

    fix nix eval tests

--- tests/test_underscore_override_extra.nix
+++ tests/test_underscore_override_extra.nix
@@ -8,7 +8,10 @@ with builtins;
 mach-nix.mkPython (baseArgsMkPython // {
   packagesExtra = [
     (mach-nix.buildPythonPackage {
-      src = "https://gitlab.com/ae-dir/web2ldap/-/archive/v1.5.97/web2ldap-v1.5.97.tar.gz";
+      src = builtins.fetchGit {
+        url = "https://code.stroeder.com/ldap/web2ldap";
+        ref = "refs/tags/v1.5.97";
+      };
       _.ldap0.buildInputs = with mach-nix.nixpkgs; [ openldap.dev cyrus_sasl.dev ];
       _.ldap0.src = builtins.fetchGit {
         url = "https://gitlab.com/ae-dir/python-ldap0";
